f295c98cbad8c80be188d23357cb96f54c2ec4ad
helenasabel <helena.b.sabel@gmail.com>
2024-04-23 17:55:55 +0200

    corrects scroll sync when new panel is added

--- resources/scripts/edition.js
+++ resources/scripts/edition.js
@@ -1,3 +1,31 @@
+window.addEventListener("WebComponentsReady", () => {
+
+// Scroll sync: add event listener to scroll button to fire function 
+  var button = document.getElementById('scroll');
+  button.addEventListener('change', scrollSync, false);
+  
+  /* If a panel is added when the scroll sync is active, click on the button
+  to activate the pb-highlight elements of the new panel */
+  pbEvents.subscribe("pb-refresh", 'transcription', (ev) => {
+     if (button.active) {
+         button.click();
+         button.click()
+         }
+    });
+});
+
+function scrollSync() {
+    var highlights = document.querySelectorAll('pb-highlight');
+    for (var i = 0; i < highlights.length; i++) {
+         if (this.active) {
+             highlights[i].removeAttribute('disabled');
+         } else {
+              highlights[i].setAttribute('disabled', 'true');
+             }
+    }
+};
+
+// Function to add the content of the metadata button available in the toolbar of each panel
 function getMetadata(popover) {
     const endpoint = document.querySelector("pb-page").getEndpoint();
     var panel = popover.parentElement;
@@ -13,18 +41,3 @@ function getMetadata(popover) {
         popover.alternate = html
     });
 };
-
-window.addEventListener("WebComponentsReady", () => {
-  var button = document.getElementById('scroll');
-
-  button.addEventListener('change', function () {
-    var highlights = document.querySelectorAll('pb-highlight');
-    for (var i = 0; i < highlights.length; i++) {
-         if (button.active) {
-             highlights[i].removeAttribute('disabled');
-         } else {
-              highlights[i].setAttribute('disabled', 'true');
-             }
-    }
-  });
-});
