fd9c21b7801ea14b5746284e8302b0213e314124
helenasabel <helena.b.sabel@gmail.com>
2024-04-25 11:21:05 +0200

    adds highlight on click

--- resources/scripts/edition.js
+++ resources/scripts/edition.js
@@ -7,11 +7,8 @@ window.addEventListener("WebComponentsReady", () => {
     buttonScroll.buttonCounterpart = buttonClick;
     buttonClick.buttonCounterpart = buttonScroll;
 
-// Scroll sync: add event listener to scroll button to fire function 
+// Adds event listener to buttons to fire respective functions
   buttonScroll.addEventListener('change', scrollSync, false);
-  
-  
-  // Click sync: add event listener to click button to fire function 
   buttonClick.addEventListener('change', clickSync, false);
   
   /* If a panel is added when the scroll sync is active, click on the button
@@ -23,12 +20,12 @@ window.addEventListener("WebComponentsReady", () => {
          }
 /* Then disable it for the panels that have “dysable sync” active */
     var disableButtons = document.querySelectorAll('.disable');
-    for (var i = 0; i < disableButtons.length; i++) {
-         if (disableButtons[i].active) {
-             disableSync(disableButtons[i])
+    disableButtons.forEach(button => {
+        if (button.active) {
+             disableSync(button)
          } 
-    }
-    });
+        })
+});
 });
 
 // Desactivate click/scroll button when selecting the other option
@@ -39,21 +36,56 @@ function disableCounterpart(button, counterpart) {
     };
 
 function clickSync(evt) {
-     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
-     }
+    disableCounterpart(this, evt.currentTarget.buttonCounterpart);
+    var segs = document.querySelectorAll('.syncSpan, pb-highlight');
+    segs.forEach(seg => {
+        seg.classList.toggle('hide')
+        })
+};
 
 function scrollSync(evt) {
     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
     var highlights = document.querySelectorAll('pb-highlight');
-    for (var i = 0; i < highlights.length; i++) {
+    highlights.forEach(pb => {
          if (this.active) {
-             highlights[i].removeAttribute('disabled');
+             pb.removeAttribute('disabled');
          } else {
-              highlights[i].setAttribute('disabled', 'true');
+             pb.setAttribute('disabled', 'true');
              }
-    }
+        })
+   
 };
 
+function highlightSeg(seg) {
+    clearHighlights();
+    var key = seg.dataset.ref;
+    var spans = document.querySelectorAll(`[data-ref="${key}"]`);
+    spans.forEach(el => {
+        el.style.backgroundColor = 'var(--pb-highlight-color)';
+        })
+};
+
+function clearHighlights() {
+    var spans = document.querySelectorAll('[data-ref]');
+    spans.forEach(span => {
+        span.style.backgroundColor = 'inherit'})
+    };
+
+// Disable sync for a specific panel
+function disableSync(button) {
+    var panel = button.parentElement;
+    var highlights = panel.querySelectorAll('pb-highlight');
+    highlights.forEach(el => {
+        if (button.active) {
+            el.setAttribute('disabled', 'true');
+            el.setAttribute('scroll', 'false');
+         } else {
+            el.removeAttribute('disabled');
+            el.setAttribute('scroll', 'true');
+            }
+        })
+   };
+   
 // Function to add the content of the metadata button available in the toolbar of each panel
 function getMetadata(popover) {
     const endpoint = document.querySelector("pb-page").getEndpoint();
@@ -70,18 +102,3 @@ function getMetadata(popover) {
         popover.alternate = html
     });
 };
-
-// Disable sync for a specific panel
-function disableSync(button) {
-    console.log('stop sync');
-    var panel = button.parentElement;
-    var highlights = panel.querySelectorAll('pb-highlight');
-    console.log(`highlights are ${highlights.length}}`)
-    for (var i = 0; i < highlights.length; i++) {
-         if (button.active) {
-             highlights[i].setAttribute('disabled', 'true');
-         } else {
-              highlights[i].removeAttribute('disabled');
-             }
-    }
-   };
