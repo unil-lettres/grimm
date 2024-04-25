3488ddec7f4335e6f33d76e86fc67089ce7fb171
helenasabel <helena.b.sabel@gmail.com>
2024-04-25 17:05:39 +0200

    sync by click complete (including highlight of closest match)

--- resources/scripts/edition.js
+++ resources/scripts/edition.js
@@ -6,86 +6,150 @@ window.addEventListener("WebComponentsReady", () => {
     // Adds button counterpart parameter to disable one button when clicking on the other
     buttonScroll.buttonCounterpart = buttonClick;
     buttonClick.buttonCounterpart = buttonScroll;
-
-// Adds event listener to buttons to fire respective functions
-  buttonScroll.addEventListener('change', scrollSync, false);
-  buttonClick.addEventListener('change', clickSync, false);
-  
-  /* If a panel is added when the scroll sync is active, click on the button
-  to activate the pb-highlight elements of the new panel */
-  pbEvents.subscribe("pb-refresh", 'transcription', (ev) => {
-     if (buttonScroll.active) {
-            buttonScroll.click();
-            buttonScroll.click()
-         }
-/* Then disable it for the panels that have “dysable sync” active */
-    var disableButtons = document.querySelectorAll('.disable');
-    disableButtons.forEach(button => {
-        if (button.active) {
-             disableSync(button)
-         } 
+    
+    // Adds event listener to buttons to fire respective functions
+    buttonScroll.addEventListener('change', scrollSync, false);
+    buttonClick.addEventListener('change', clickSync, false);
+    
+    /* If a panel is added or changed, click on the sync buttons
+     to enable the appropriate elements in the new panel */
+    pbEvents.subscribe("pb-panel", 'transcription', (ev) => {
+        const buttons =[buttonScroll, buttonClick];
+        buttons.forEach(button => {
+            if (button.active) {
+                button.click();
+                button.click()
+            };
+        });
+        // Then disable it for the panels that have “dysable sync” active
+        var disableButtons = document.querySelectorAll('.disable');
+        disableButtons.forEach(button => {
+            if (button.active) {
+                disableSync(button)
+            }
         })
-});
+    });
 });
 
 // Desactivate click/scroll button when selecting the other option
 function disableCounterpart(button, counterpart) {
     if (button.active && counterpart.active) {
         counterpart.click();
-        };
     };
+};
 
+/* Function to sync by clicking on the spans (which are not visible by default: pb-highlight elements are).
+  N.B.: When adding a new panel, the spans are not visible, so we cannot just get all spans and pb-highlight elements
+  and just use classList.toggle for all */
 function clickSync(evt) {
     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
-    var segs = document.querySelectorAll('.syncSpan, pb-highlight');
-    segs.forEach(seg => {
-        seg.classList.toggle('hide')
-        })
+    clearHighlights();
+    // First hide/show pb-highlight elements accordingly
+    var pbs = document.querySelectorAll('pb-highlight');
+    pbs.forEach(pb => {
+        if (this.active) {
+            pb.classList.add('hide');
+        } else {
+            pb.classList.remove('hide')
+        }
+    })
+    // Hide/show the spans for clicking. The already contain an inline event listener
+    var spans = document.querySelectorAll('.syncSpan')
+    spans.forEach(span => {
+        if (this.active) {
+            span.classList.remove('hide');
+        } else {
+            span.classList.add('hide')
+        }
+    })
 };
 
 function scrollSync(evt) {
     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
+    clearHighlights();
     var highlights = document.querySelectorAll('pb-highlight');
     highlights.forEach(pb => {
-         if (this.active) {
-             pb.removeAttribute('disabled');
-         } else {
-             pb.setAttribute('disabled', 'true');
-             }
-        })
-   
+        if (this.active) {
+            pb.removeAttribute('disabled');
+        } else {
+            pb.setAttribute('disabled', 'true');
+        }
+    })
 };
 
 function highlightSeg(seg) {
     clearHighlights();
     var key = seg.dataset.ref;
-    var spans = document.querySelectorAll(`[data-ref="${key}"]`);
+    var spans = document.querySelectorAll(`[data-ref = "${key}"]`);
     spans.forEach(el => {
         el.style.backgroundColor = 'var(--pb-highlight-color)';
+        el.scrollIntoView({
+            block: "center", behavior: "instant"
         })
+    })
+    closestMatch(key);
 };
+
+function closestMatch(key) {
+    const source = key.split("_").pop();
+    var panels = document.querySelectorAll('pb-panel');
+    panels.forEach(panel => {
+        const values = []
+        const spans = panel.querySelectorAll('*[data-ref]');
+        spans.forEach(span => {
+            var ref = span.dataset.ref
+            values.push(ref.split("_").pop())
+            });
+        if (values.includes(source) == false) {
+            const closest = values.reduce((a, b) => {
+                return Math.abs(b - source) < Math.abs(a - source) ? b : a;
+                });
+            spans.forEach(span => {
+                if (span.dataset.ref.split("_").pop() == closest) {
+                    span.style.backgroundColor = 'red';
+                    span.scrollIntoView({block: "center", behavior: "instant"})
+                    }
+                })    
+            }
+        })
+    };
 
 function clearHighlights() {
     var spans = document.querySelectorAll('[data-ref]');
     spans.forEach(span => {
-        span.style.backgroundColor = 'inherit'})
-    };
+        span.style.backgroundColor = 'inherit'
+    })
+};
 
 // Disable sync for a specific panel
 function disableSync(button) {
+    clearHighlights();
     var panel = button.parentElement;
     var highlights = panel.querySelectorAll('pb-highlight');
-    highlights.forEach(el => {
+    const syncModeClick = document.getElementById('click');
+    const syncModeScroll = document.getElementById('scroll')
+    highlights.forEach(pb => {
         if (button.active) {
-            el.setAttribute('disabled', 'true');
-            el.setAttribute('scroll', 'false');
-         } else {
-            el.removeAttribute('disabled');
-            el.setAttribute('scroll', 'true');
+            pb.setAttribute('disabled', 'true');
+            pb.removeAttribute('scroll');
+            pb.classList.remove('hide')
+        } else if (syncModeScroll.active) {
+            pb.removeAttribute('disabled');
+            pb.setAttribute('scroll', 'true');
+        } else if (syncModeClick.active) {
+            pb.classList.add('hide')
             }
-        })
-   };
-   
+    });
+    var spans = panel.querySelectorAll('.syncSpan');
+    spans.forEach(span => {
+        if (button.active) {
+            span.classList.add('hide')
+        } else if (syncModeClick.active) {
+            span.classList.remove('hide')
+        }
+    })
+};
+
 // Function to add the content of the metadata button available in the toolbar of each panel
 function getMetadata(popover) {
     const endpoint = document.querySelector("pb-page").getEndpoint();
@@ -94,11 +158,10 @@ function getMetadata(popover) {
     var number = panel.getAttribute('active');
     var url = `${endpoint}/api/metadata/${doc}/${number}`;
     console.log(`fetching metadata from ${url}`);
-    fetch(url)
-    .then(function(response) {
+    fetch(url).then(function (response) {
         return response.text();
     }).
-    then(function(html) {
+    then(function (html) {
         popover.alternate = html
     });
 };
