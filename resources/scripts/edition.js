67ed8813b762847da263e548737f0f359c5fe922
helenasabel <helena.b.sabel@gmail.com>
2024-05-30 15:46:39 +0200

    changes sync functions to address multiple keys in the same segment

--- resources/scripts/edition.js
+++ resources/scripts/edition.js
@@ -7,37 +7,17 @@ window.addEventListener("load", () => {
     buttonScroll.buttonCounterpart = buttonClick;
     buttonClick.buttonCounterpart = buttonScroll;
     
+    // Adds mode parameter to add appropriate event listener to spans
+    buttonScroll.mode = 'scroll';
+    buttonClick.mode = 'click';
+    
     // Adds event listener to buttons to fire respective functions
-    buttonScroll.addEventListener('change', scrollSync, false);
-    buttonClick.addEventListener('change', clickSync, false);
+    buttonScroll.addEventListener('change', spanSync, false);
+    buttonClick.addEventListener('change', spanSync, false);
     
     // Click scroll button as default
     buttonScroll.click();
     
-    /* If a panel is added or changed, click on the sync buttons
-     to enable the appropriate elements in the new panel */
-    pbEvents.subscribe("pb-panel", 'transcription', (ev) => {
-        const buttons = [buttonScroll, buttonClick];
-        buttons.forEach(button => {
-            if (button.active) {
-                button.click();
-                button.click()
-            };
-        });
-        // Then disable it for the panels that have “disable sync” active
-        disablePanels();
-
-    });
-    
-    /* With pb-highlight enabled, fire closest match function to higlight segment when there is no match;
-    then clean it off */
-    pbEvents.subscribe('pb-highlight-on', null, (ev) => {
-        var key = ev.detail.id;
-        closestMatch(key);
-        });
-    pbEvents.subscribe('pb-highlight-off', null, (ev) => {
-         clearHighlights();
-        });
     
     pbEvents.subscribe('pb-end-update', 'panels', () => {
         const grid = document.querySelector('pb-grid');
@@ -63,7 +43,6 @@ function disablePanels() {
     disableButtons.forEach(button => {
         if (button.active) {
             disableSync(button);
-            console.log('Disable sync gets fired')
         };
     })
 };
@@ -75,59 +54,39 @@ function disableCounterpart(button, counterpart) {
     };
 };
 
-/* Function to sync by clicking on the spans (which are not visible by default: pb-highlight elements are).
-  N.B.: When adding a new panel, the spans are not visible, so we cannot just get all spans and pb-highlight elements
-  and just use classList.toggle for all */
-function clickSync(evt) {
+/* Function to sync by clicking on the spans */
+function spanSync(evt) {
     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
     clearHighlights();
-    // First hide/show pb-highlight elements accordingly
-    var pbs = document.querySelectorAll('pb-highlight');
-    pbs.forEach(pb => {
-        if (this.active) {
-            pb.classList.add('hide');
-        } else {
-            pb.classList.remove('hide')
-        }
-    });
-    // Hide/show the spans for clicking. The already contain an inline event listener
-    var spans = document.querySelectorAll('.syncSpan')
+    // Add eventlistener
+    var spans = document.querySelectorAll('*[data-ref]')
     spans.forEach(span => {
-        if (this.active) {
-            span.classList.remove('hide');
-        } else {
-            span.classList.add('hide')
-        }
+        switch(evt.currentTarget.mode) {
+            case "click":
+                span.removeEventListener('mouseover', highlightSeg, false);
+                span.addEventListener('click', highlightSeg, false);
+                break;
+            case "scroll":
+                span.removeEventListener('click', highlightSeg, false);
+                span.addEventListener('mouseover', highlightSeg, false);
+            }
     });
     disablePanels();
 };
 
-// Enable scroll synchronization (based on pb-hightlight elements)
-function scrollSync(evt) {
-    disableCounterpart(this, evt.currentTarget.buttonCounterpart);
+function highlightSeg() {
     clearHighlights();
-    var highlights = document.querySelectorAll('pb-highlight');
-    highlights.forEach(pb => {
-        if (this.active) {
-            pb.removeAttribute('disabled');
-        } else {
-            pb.setAttribute('disabled', 'true');
-        }
-    });
-    disablePanels();
-};
-
-function highlightSeg(seg) {
-    clearHighlights();
-    var key = seg.dataset.ref;
-    var spans = document.querySelectorAll(`[data-ref = "${key}"]`);
-    spans.forEach(el => {
-        el.style.backgroundColor = 'var(--pb-highlight-color)';
-        el.scrollIntoView({
+    var refs = this.dataset.ref.match(/\w+/g);
+    refs.forEach(ref => {
+        var spans = document.querySelectorAll(`span:not(.disabled).${ref}`);    
+        spans.forEach(el => {
+            el.style.backgroundColor = 'var(--pb-highlight-color)';
+            el.scrollIntoView({
             block: "center", behavior: "instant"
-        })
-    })
-    closestMatch(key);
+            })
+        });
+        closestMatch(ref);
+    });
 };
 
 /*When there is no exact match, show square brackets at closest position*/
@@ -136,21 +95,27 @@ function closestMatch(key) {
     var panels = document.querySelectorAll('pb-panel');
     panels.forEach(panel => {
         const values = []
-        const spans = panel.querySelectorAll('*[data-key]');
+        const spans = panel.querySelectorAll('*[data-key]:not(.disabled)');
         spans.forEach(span => {
-            var ref = span.dataset.key
-            values.push(ref.split("_").pop())
+            var refs = span.dataset.key.match(/\w+/g);
+            refs.forEach(ref => {
+                values.push(ref.split("_").pop())
+                })
             });
         if (values.includes(source) == false) {
             const closest = values.reduce((a, b) => {
                 return Math.abs(b - source) < Math.abs(a - source) ? b : a;
                 });
             spans.forEach(span => {
-                if (span.dataset.key.split("_").pop() == closest) {
-                    span.classList.remove('hide');
-                    span.style.backgroundColor = 'var(--pb-highlight-color)';
-                    span.scrollIntoView({block: "center", behavior: "instant"})
-                    }
+                var refs = span.dataset.key.match(/\w+/g);
+                refs.forEach(ref => {
+                   if (ref.split("_").pop() == closest) {
+                        span.classList.remove('hide');
+                        span.style.backgroundColor = 'var(--pb-highlight-color)';
+                        span.scrollIntoView({block: "center", behavior: "instant"})
+                        }
+                    })
+
             })
         }
     })
@@ -171,32 +136,13 @@ function clearHighlights() {
 function disableSync(button) {
     clearHighlights();
     var panel = button.parentElement;
-    var highlights = panel.querySelectorAll('pb-highlight');
-    const syncModeClick = document.getElementById('click');
-    const syncModeScroll = document.getElementById('scroll')
-    highlights.forEach(pb => {
-        if (button.active) {
-            pb.setAttribute('disabled', 'true');
-            pb.removeAttribute('scroll');
-            pb.classList.remove('hide');
-        } else if (syncModeScroll.active) {
-            pb.removeAttribute('disabled');
-            pb.setAttribute('scroll', 'true');
-            pb.classList.remove('hide');
-        } else if (syncModeClick.active) {
-            pb.classList.add('hide')
-            }
-    });
-    var spans = panel.querySelectorAll('.syncSpan');
+    var spans = panel.querySelectorAll('[data-ref]');
     spans.forEach(span => {
         if (button.active) {
-            span.classList.add('hide');
-        } else if (syncModeClick.active) {
-            span.classList.remove('hide');
+            span.removeEventListener('click', highlightSeg, false);
+            span.removeEventListener('mouseover', highlightSeg, false);
+            span.classList.add('disabled')
         }
-        else if (syncModeScroll.active) {
-            span.classList.add('hide');
-            }
     })
 };
 
