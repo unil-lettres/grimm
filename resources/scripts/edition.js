c24df98d155ad8f6b73f03435eacdcbd85c09f77
helenasabel <helena.b.sabel@gmail.com>
2024-05-31 17:26:30 +0200

    corrected issues when adding a new panel

--- resources/scripts/edition.js
+++ resources/scripts/edition.js
@@ -7,17 +7,12 @@ window.addEventListener("load", () => {
     buttonScroll.buttonCounterpart = buttonClick;
     buttonClick.buttonCounterpart = buttonScroll;
     
-    // Adds mode parameter to add appropriate event listener to spans
-    buttonScroll.mode = 'scroll';
-    buttonClick.mode = 'click';
-    
     // Adds event listener to buttons to fire respective functions
     buttonScroll.addEventListener('change', spanSync, false);
     buttonClick.addEventListener('change', spanSync, false);
     
     // Click scroll button as default
     buttonScroll.click();
-    
     
     pbEvents.subscribe('pb-end-update', 'panels', () => {
         const grid = document.querySelector('pb-grid');
@@ -36,39 +31,61 @@ window.addEventListener("load", () => {
             });
         });
     });
+    pbEvents.subscribe("pb-panel", 'transcription', (ev) => {
+        clearHighlights();
+        var mode = retrieveSyncMode();
+        addEvents(mode);
+    });
 });
 
-function disablePanels() {
-    var disableButtons = document.querySelectorAll('.disable');
-    disableButtons.forEach(button => {
-        if (button.active) {
-            disableSync(button);
-        };
-    })
-};
 
-// Desactivate click/scroll button when selecting the other option
+/* Desactivate click/scroll button when selecting the other option
+Buttons should probably be radio buttons to enable this behaviour automatically */
 function disableCounterpart(button, counterpart) {
     if (button.active && counterpart.active) {
         counterpart.click();
     };
 };
 
-/* Function to sync by clicking on the spans */
+function addEvents(mode) {
+    var panels = document.querySelectorAll('pb-panel:not(.disabled)');
+    if (panels) {
+        panels.forEach(panel => {
+            var spans = panel.querySelectorAll('*[data-ref]')
+            spans.forEach(span => {
+                span.addEventListener(mode, highlightSeg, false);
+            })
+        });
+    }
+};
+
+/* Function to sync the spans */
 function spanSync(evt) {
+    // Disable the other button and clear any highlights from previous mode
     disableCounterpart(this, evt.currentTarget.buttonCounterpart);
     clearHighlights();
-    // Add eventlistener
+    // Add eventlistener to spans based on mode
+    var mode = retrieveSyncMode()
     var spans = document.querySelectorAll('*[data-ref]')
     spans.forEach(span => {
-        switch(evt.currentTarget.mode) {
+        switch(mode) {
             case "click":
-                span.removeEventListener('mouseover', highlightSeg, false);
-                span.addEventListener('click', highlightSeg, false);
+                if (this.active) {
+                    span.removeEventListener('mouseover', highlightSeg, false);
+                    span.addEventListener('click', highlightSeg, false);
+                }
+                else {
+                    span.removeEventListener('click', highlightSeg, false);
+                    }
                 break;
-            case "scroll":
-                span.removeEventListener('click', highlightSeg, false);
-                span.addEventListener('mouseover', highlightSeg, false);
+            case "mouseover":
+                if (this.active) {
+                    span.removeEventListener('click', highlightSeg, false);
+                    span.addEventListener('mouseover', highlightSeg, false);
+                    }
+                else {
+                    span.removeEventListener('mouseover', highlightSeg, false);
+                }
             }
     });
     disablePanels();
@@ -82,20 +99,22 @@ function highlightSeg() {
         spans.forEach(el => {
             el.style.backgroundColor = 'var(--pb-highlight-color)';
             el.scrollIntoView({
-            block: "center", behavior: "instant"
+            block: "center", behavior: "smooth"
             })
         });
-        closestMatch(ref);
     });
+    /* We fire the highlightening of segments that do not match just with one of the keys.
+    Otherwise, we might highlight more that one segment and it will be a bit confusing*/
+    closestMatch(refs[0]);
 };
 
 /*When there is no exact match, show square brackets at closest position*/
 function closestMatch(key) {
     const source = key.split("_").pop();
-    var panels = document.querySelectorAll('pb-panel');
+    var panels = document.querySelectorAll('pb-panel:not(.disabled)');
     panels.forEach(panel => {
         const values = []
-        const spans = panel.querySelectorAll('*[data-key]:not(.disabled)');
+        const spans = panel.querySelectorAll('*[data-key]');
         spans.forEach(span => {
             var refs = span.dataset.key.match(/\w+/g);
             refs.forEach(ref => {
@@ -121,8 +140,18 @@ function closestMatch(key) {
     })
 };
 
+/* Check which panels have the synchronization disabled, and act accordingly */
+function disablePanels() {
+    var disableButtons = document.querySelectorAll('.disable');
+    disableButtons.forEach(button => {
+        if (button.active) {
+            disableSync(button);
+        };
+    })
+};
+
 function clearHighlights() {
-    var spans = document.querySelectorAll('[data-ref],.noMatch');
+    var spans = document.querySelectorAll('[data-ref], .noMatch');
     spans.forEach(span => {
         span.style.backgroundColor = 'inherit'
     });
@@ -132,17 +161,38 @@ function clearHighlights() {
         })
 };
 
+// Function to check which mode is active
+function retrieveSyncMode() {
+    const buttonScroll = document.getElementById('scroll');
+    const buttonClick = document.getElementById('click');
+    if (buttonScroll.active) {
+        return 'mouseover'
+        }
+    else if (buttonClick.active) {
+        return 'click'
+        }
+    };
+
 // Disable sync for a specific panel
 function disableSync(button) {
     clearHighlights();
     var panel = button.parentElement;
-    var spans = panel.querySelectorAll('[data-ref]');
-    spans.forEach(span => {
-        if (button.active) {
-            span.removeEventListener('click', highlightSeg, false);
-            span.removeEventListener('mouseover', highlightSeg, false);
-            span.classList.add('disabled')
+    if (button.active) {
+        panel.classList.add('disabled')
         }
+    else {
+        panel.classList.remove('disabled')
+        };
+    var spans = panel.querySelectorAll('[data-ref], .noMatch');
+    var mode = retrieveSyncMode()
+    spans.forEach(span => {
+        if (button.active && mode) {
+            span.removeEventListener(mode, highlightSeg, false);
+            span.classList.add('disabled')
+        } else if (mode) {
+            span.addEventListener(mode, highlightSeg, false);
+            span.classList.remove('disabled')
+            }
     })
 };
 
