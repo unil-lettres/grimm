1a4a1da9874cf7856bccabeebf208fab3db72e21
duncdrum <d.paterson@me.com>
2024-06-18 23:05:41 +0200

    test(cypress): scaffold

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -7,4 +7,43 @@ describe('index page', () => {
     cy.get('.tp-title > pb-i18n')
       .contains('Grimm')
   })
+
+  it.skip('should show stories without selecting a language', () => {
+    cy.get('.tp-title > pb-i18n')
+      .contains('Grimm')
+  })
+
+  it.skip('should perform search over corpus', () => {
+    cy.get('.tp-title > pb-i18n')
+      .contains('Grimm')
+  })
+
+  it.skip('should switch languages', () => {
+    cy.get('.tp-title > pb-i18n')
+      .contains('Grimm')
+  })
+
+  describe('search facettes', () => {
+
+    it.skip('should have working title search', () => {
+      cy.get('.tp-title > pb-i18n')
+        .contains('Grimm')
+    })
+
+    it.skip('should show languages', () => {
+      cy.get('.tp-title > pb-i18n')
+        .contains('Grimm')
+    })
+
+    it.skip('should show translators', () => {
+      cy.get('.tp-title > pb-i18n')
+        .contains('Grimm')
+    })
+
+    it.skip('should show century', () => {
+      cy.get('.tp-title > pb-i18n')
+        .contains('Grimm')
+    })
+  })
+
 })
